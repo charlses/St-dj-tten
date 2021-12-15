f7241ea874415286bbf3f55292e3b8abd4387de8
Davit Tavadze <davit@scriptify.se>
2021-12-15 17:32:40 +0100

    commit

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,12 @@
 const functions = require('firebase-functions')
+const cors = require('cors')
+
+const express = require('express')
+const app = express()
+
+exports.corsFuction = functions.https.onRequest((request, response) => {
+  app.use(cors({ origin: true }))
+})
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
--- pages-sections/sections-page/SectionForm.js
+++ pages-sections/sections-page/SectionForm.js
@@ -14,6 +14,7 @@ import CardBody from 'components/Card/CardBody.js'
 import CustomInput from 'components/CustomInput/CustomInputWhite.js'
 import axios from 'axios'
 import headersStyle from 'styles/jss/nextjs-material-kit-pro/pages/sectionsSections/headersStyle.js'
+import e from 'cors'
 
 const useStyles = makeStyles(headersStyle)
 
@@ -26,139 +27,154 @@ export default function SectionForm() {
   const [newService, setNewService] = useState('')
   const [newDate, setNewDate] = useState('')
 
-  const sendPostRequest = async () => {
-    try {
-      const res = await axios.all([
-        axios({
-          method: 'post',
-          url: process.env.limeApiUrlPerson,
-          headers: {
-            'Accept': 'application/hal+json',
-            'x-api-key': process.env.limeApiKey
-          }
-        }),
-        axios({
-          method: 'post',
-          url: process.env.limeApiUrlCompany,
-          data: {
-            name: newName,
-            phone: newPhone,
-            email: newEmail
-          },
-          headers: {
-            'Accept': 'application/hal+json',
-            'x-api-key': process.env.limeApiKey
-          }
-        }),
-        axios({
-          method: 'post',
-          url: process.env.limeApiUrlDeal,
-          headers: {
-            'Accept': 'application/hal+json',
-            'x-api-key': process.env.limeApiKey
-          }
-        })
-      ])
+  const headers = () => {
+    return {
+      'Accept': 'application/hal+json',
+      'x-api-key': process.env.limeApiKey
+    }
+  }
 
-      return res.status === '200'
+  const sendPostRequest = async (e) => {
+    try {
+      e.preventDefault()
+      const res1 = await axios({
+        method: 'post',
+        url: process.env.limeApiUrlCompany,
+        headers: headers(),
+        data: {}
+      })
+      const res3 = await axios({
+        method: 'post',
+        url: process.env.limeApiUrlPerson,
+        headers: headers(),
+        data: {
+          firstname: newName,
+          email: newEmail,
+          phone: newPhone
+        }
+      })
+      const res3Data = res3.data
+      console.log(res3Data._id)
+      return res3.status === '200'
     } catch (err) {
       console.log(err.message)
-      return false
     }
   }
 
   return (
     // we've set the className to cd-section so we can make smooth scroll to it
 
-    <GridContainer>
-      <GridItem
-        xs={12}
-        sm={10}
-        md={10}
-        className={classNames(classes.mlAuto, classes.mrAuto)}
-      >
-        <Card raised className={classes.card}>
-          <CardBody formHorizontal>
-            <h5 className={(classes.title, classes.textCenter)}>
-              Skicka en offerförfrågan så kontaktar vi dig inom 1 timme
-            </h5>
-            <form>
-              <GridContainer>
-                <GridItem xs={12} sm={6} md={2}>
-                  <CustomInput
-                    id='float'
-                    required
-                    onChange={(e) => setNewName(e.target.value)}
-                    value={newName}
-                    labelText='Ditt namn'
-                    formControlProps={{
-                      fullWidth: true,
-                      className: classes.formControl
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={6} md={2}>
-                  <CustomInput
-                    id='float'
-                    required
-                    onChange={(e) => setNewEmail(e.target.value)}
-                    labelText='E-post'
-                    value={newEmail}
-                    formControlProps={{
-                      fullWidth: true,
-                      className: classes.formControl
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={6} md={2}>
-                  <CustomInput
-                    id='float'
-                    labelText='Telefonnummer'
-                    onChange={(e) => setNewPhone(e.target.value)}
-                    value={newPhone}
-                    required
-                    formControlProps={{
-                      fullWidth: true,
-                      className: classes.formControl
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={6} md={2}>
-                  <CustomInput
-                    id='float'
-                    required
-                    onChange={(e) => setNewService(e.target.value)}
-                    value={newService}
-                    labelText='Välj en tjänst'
-                    formControlProps={{
-                      fullWidth: true,
-                      className: classes.formControl
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={6} md={2}>
-                  <CustomInput
-                    id='float'
-                    required
-                    onChange={(e) => setNewDate(e.target.value)}
-                    value={newDate}
-                    labelText='Datumn'
-                    formControlProps={{
-                      fullWidth: true,
-                      className: classes.formControl
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={6} md={2}>
-                  <Button block color='primary' className={classes.button}>
-                    Skicka
-                  </Button>
-                </GridItem>
-              </GridContainer>
-            </form>
-          </CardBody>
-        </Card>
-      </GridItem>
-    </GridContainer>
+    <form onSubmit={sendPostRequest}>
+      <input
+        type='text'
+        onChange={(e) => setNewName(e.target.value)}
+        value={newName}
+        placeholder='namn'
+      />
+      <input
+        type='text'
+        onChange={(e) => setNewEmail(e.target.value)}
+        value={newEmail}
+        placeholder='e-post'
+      />
+      <input
+        type='text'
+        onChange={(e) => setNewPhone(e.target.value)}
+        value={newPhone}
+        placeholder='telefonnummer'
+      />
+      <button> Skicka </button>
+    </form>
+
+    // <GridContainer>
+    //   <GridItem
+    //     xs={12}
+    //     sm={10}
+    //     md={10}
+    //     className={classNames(classes.mlAuto, classes.mrAuto)}
+    //   >
+    //     <Card raised className={classes.card}>
+    //       <CardBody formHorizontal>
+    //         <h5 className={(classes.title, classes.textCenter)}>
+    //           Skicka en offerförfrågan så kontaktar vi dig inom 1 timme
+    //         </h5>
+    //         <form>
+    //           <GridContainer>
+    //             <GridItem xs={12} sm={6} md={2}>
+    //               <CustomInput
+    //                 id='float'
+    //                 required
+    //                 onChange={(e) => setNewName(e.target.value)}
+    //                 value={newName}
+    //                 labelText='Ditt namn'
+    //                 formControlProps={{
+    //                   fullWidth: true,
+    //                   className: classes.formControl
+    //                 }}
+    //               />
+    //             </GridItem>
+    //             <GridItem xs={12} sm={6} md={2}>
+    //               <CustomInput
+    //                 id='float'
+    //                 required
+    //                 onChange={(e) => setNewEmail(e.target.value)}
+    //                 labelText='E-post'
+    //                 value={newEmail}
+    //                 formControlProps={{
+    //                   fullWidth: true,
+    //                   className: classes.formControl
+    //                 }}
+    //               />
+    //             </GridItem>
+    //             <GridItem xs={12} sm={6} md={2}>
+    //               <CustomInput
+    //                 id='float'
+    //                 labelText='Telefonnummer'
+    //                 onChange={(e) => setNewPhone(e.target.value)}
+    //                 value={newPhone}
+    //                 required
+    //                 formControlProps={{
+    //                   fullWidth: true,
+    //                   className: classes.formControl
+    //                 }}
+    //               />
+    //             </GridItem>
+    //             <GridItem xs={12} sm={6} md={2}>
+    //               <CustomInput
+    //                 id='float'
+    //                 required
+    //                 onChange={(e) => setNewService(e.target.value)}
+    //                 value={newService}
+    //                 labelText='Välj en tjänst'
+    //                 formControlProps={{
+    //                   fullWidth: true,
+    //                   className: classes.formControl
+    //                 }}
+    //               />
+    //             </GridItem>
+    //             <GridItem xs={12} sm={6} md={2}>
+    //               <CustomInput
+    //                 id='float'
+    //                 required
+    //                 onChange={(e) => setNewDate(e.target.value)}
+    //                 value={newDate}
+    //                 labelText='Datumn'
+    //                 formControlProps={{
+    //                   fullWidth: true,
+    //                   className: classes.formControl
+    //                 }}
+    //               />
+    //             </GridItem>
+    //             <GridItem xs={12} sm={6} md={2}>
+    //               <Button block color='primary' className={classes.button}>
+    //                 Skicka
+    //               </Button>
+    //             </GridItem>
+    //           </GridContainer>
+    //         </form>
+    //       </CardBody>
+    //     </Card>
+    //   </GridItem>
+    // </GridContainer>
   )
 }
